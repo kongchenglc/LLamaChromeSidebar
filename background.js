933dbdc7f88f3f91e456e71f07d8e273156067e9
kongchenglc <kongchenglc@gmail.com>
2024-10-06 19:39:20 -0400

    chore: init

---
+++ background.js
@@ -0,0 +1,7 @@
+const color = "#3aa757";
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({ color });
+    console.log(`[Coloring] default background color is set to: ${color}`);
+});
+
