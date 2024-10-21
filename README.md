426b63968c3501fe9ae3c3b1fe3f083167853fe4
kongchenglc <kongchenglc@gmail.com>
2024-10-21 16:05:30 -0400

    feat: chrome extension init

---
+++ README.md
@@ -0,0 +1,1 @@
+# LLM Chrome Siderbar Extension
--- background.js
+++
@@ -1,7 +0,0 @@
-const color = "#3aa757";
-
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ color });
-    console.log(`[Coloring] default background color is set to: ${color}`);
-});
-
